e2e817e0bcc5029d7144f20dd5503b3343bca359
echizenyayota <virtualmisso30@gmail.com>
2024-02-01 17:07:07 +0900

    Place the necessary elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 const App = () => {
   return (
     <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Echizenya</p>
+        </nav>
+      </section>
 
     </div>
   );
