91cd542b95a12980a5d8d8780de98a94da9df26a
echizenyayota <virtualmisso30@gmail.com>
2024-02-11 17:44:13 +0900

    Save the message by the useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [message, setMessage] = useState(null);
 
   const getMessages = async() => {
 
@@ -16,6 +20,7 @@ const App = () => {
       const response = await fetch('http://localhost:8000/completions', options);
       const data = await response.json();
       console.log(data);
+      setMessage(data.choices[0].message);
     } catch(err) {
       console.log(err);
     }
